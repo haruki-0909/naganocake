35448dd7926eb0613da8683506b31fbad4e46a3a
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2022-11-23 14:14:56 +0000

    [修正]customerのdbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
  scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about', as: :about
+
+    get 'customers/mypage' => 'customers#show', as: :public_customer
  end
 
 

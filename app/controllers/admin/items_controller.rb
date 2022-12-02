9ceb6173bf50a86f79c35309d0316b0179175262
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2022-12-02 12:20:46 +0000

    [Add]投稿編集画面のレイアウト完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -31,7 +31,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-      params.require(:item).permit(:genre_id, :name, :introduction, :price)
+      params.require(:item).permit(:genre_id, :name, :introduction, :price, :image)
   end
 
 

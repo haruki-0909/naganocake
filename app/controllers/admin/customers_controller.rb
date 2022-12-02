6174c991a2545b213e02a37bc9189e1f0fc6505b
EC2 Default User <hrk0911tkym@docomo.ne.jp>
2022-12-02 15:50:09 +0000

    [Add]会員詳細画面追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,5 +2,18 @@ class Admin::CustomersController < ApplicationController
 
 
   def index
+      @customer = Customer.page(params[:page])
   end
+
+  def show
+      @customer = Customer.find(params[:id])
+  end
+
+  private
+
+  def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
+
+
 end

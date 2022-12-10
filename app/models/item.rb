d950fcda545e807faf1f22c23e238ac2909b1c3d
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2022-12-10 08:31:17 +0000

    [Add]カート画面実装途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
-  def add_tax_price
-    (self.price * 1.08).round
+  def with_tax_price
+    (price * 1.1).floor
   end
 
   has_many :cart_item, dependent: :destroy

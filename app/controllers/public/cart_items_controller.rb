c84085235ffc19af78657d49d90d44e758f433f9
EC2 Default User <hrk0911tkym@docomo.ne.jp>
2022-12-10 03:20:28 +0000

    [Add]商品詳細ページ実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,13 @@
 class Public::CartItemsController < ApplicationController
-  
-  
-  
-  
-   private
-  def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+   
+  def create
+      @cart_item = current_user.cart_items.build(cart_item_params)
+      @cart_item.save
   end
-  
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :image_id, :amount)
+  end
+
 end

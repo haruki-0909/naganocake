0b72ffa30bf588306f13ae42d66a8befcebee9dc
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2022-12-17 19:38:36 +0000

    [End]実装完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,5 @@
 class Public::OrdersController < ApplicationController
 
-
   def new
       @order = Order.new
   end
@@ -9,7 +8,13 @@ class Public::OrdersController < ApplicationController
       @order = Order.new(order_params)
       @cart_items = CartItem.all
       @total = 0
-
+      if params[:order][:address_number] == "1"
+         @order.name = current_customer.last_name + current_customer.first_name
+         @order.address = current_customer.address
+         @order.postal_code = current_customer.postal_code
+      elsif params[:order][:address_number] == "2"
+        @order = Order.new(order_params)
+      end
   end
 
   def create
@@ -25,11 +30,11 @@ class Public::OrdersController < ApplicationController
         order_detail.save
         end
         redirect_to orders_complete_path
-        current_customer.cart_items.destroy_all
 
   end
 
   def complete
+      current_customer.cart_items.destroy_all
   end
 
   def show
@@ -47,5 +52,7 @@ class Public::OrdersController < ApplicationController
   def order_params
       params.require(:order).permit(:customer_id, :postage, :billing_amount, :payment_type, :postal_code, :address, :name)
   end
-
+  def address_params
+  params.require(:order).permit(:name, :address, :postal_code)
+  end
 end
